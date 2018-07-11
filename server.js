bc2eedded596a5ac96bdfef3bdf7e451ddd1047d
Jessamyn McTwigan <jessamyn.mctwigan@gmail.com>
2018-07-11 09:22:21 -0500

    up and running

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Pokemon App!')
+});
+
+
+app.listen(3000, () => {
+  console.log('I am listening on port 3000');
+});
+
+// const bodyParser = require('body-parser');
+// const methodOverride = require('method-override');
+// app.use(methodOverride('_method'));
+// const Fruits = require('./models/fruits')
+// app.use(bodyParser.urlencoded({extended: false}))
+// app.get('/fruits', (req, res) => {
+// res.render('index.ejs', {
+// fruits: Fruits
+//   });
+// });
+//
+// app.post('/fruits', (req, res) => {
+// if(req.body.readyToEat === 'on') {
+//   req.body.readyToEat = true;
+// } else {
+//   req.body.readyToEat = false;
+// }
+//
+// Fruits.push(req.body);
+//
+// console.log(req.body, 'this is req.body, shoud be the form info');
+//   res.redirect('/fruits');
+// })
+//
+// app.get('/fruits/new', (req, res) => {
+//   res.render('new.ejs');
+// });
+//
+// app.get('/fruits/:index/edit', function(req, res){
+// 	res.render(
+// 		'edit.ejs', //render views/edit.ejs
+// 		{ //pass in an object that contains
+// 			fruit: fruits[req.params.index], //the fruit object
+// 			index: req.params.index //... and its index in the array
+// 		}
+// 	);
+// });
+//
+// // show route
+// app.get('/fruits/:index', (req, res) => {
+// res.render('show.ejs', {
+//   fruit: Fruits[req.params.index]
+// })
+// });
+//
+//  // delete route
+// app.delete('/fruits/:index', (req, res) => {
+// 	Fruits.splice(req.params.index, 1); //remove the item from the array
+// 	res.redirect('/fruits');  //redirect back to index route
+// });
