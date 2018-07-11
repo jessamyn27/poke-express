b62f6219aa9e89c875888e734c34892ad9ce81d9
Jessamyn McTwigan <jessamyn.mctwigan@gmail.com>
2018-07-11 10:36:26 -0500

    Commit 8 - added dynamic anchor tags to index.ejs

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ app.get('/pokemon', (req,res)=>{
     });
 })
 
+app.get('/pokemon/:id', (req,res)=>{
+    res.render('show.ejs', {
+      pokemon: Pokemon[req.params.id]
+    })
+    });
+
+
 app.listen(3000, () => {
   console.log('I am listening on port 3000');
 });
